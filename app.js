3adbf4e0c34b5279469c2c7e7f64f588e453fa72
eldrige <peldrige8@gmail.com>
2021-07-08 17:56:41 +0100

    initial post route

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 app.get('/', (req, res) => {
   res.send('Natours API');
 });
