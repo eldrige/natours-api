1dcca8cbed6bb92318f71124402b3e2bc7b0c25e
eldrige <peldrige8@gmail.com>
2021-07-08 17:49:24 +0100

    first get request

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ app.use(express.json());
 
 const PORT = 2000;
 
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.json({
     data: {
+      results: tours.length,
       tours,
     },
   });
 });
 
-app.listen(PORT, () =>
-  console.log(`Server is accepting request at : ${PORT} `)
-);
+app.get('/', (req, res) => {
+  res.send('Natours API');
+});
+
+app.listen(PORT, () => console.log(`Server is accepting request at: ${PORT} `));
