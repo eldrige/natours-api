cf65ad2f30a63b02d513b682d615505640432226
eldrige <peldrige8@gmail.com>
2021-08-10 22:02:59 +0100

    Create error handling middleware

--- app.js
+++ app.js
@@ -11,15 +11,26 @@ app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours/', tourRouter);
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Couldnot find ${req.originalUrl} on this server`,
-  });
-});
-
 app.get('/', (req, res) => {
   res.send('Natours API');
+});
+
+app.all('*', (req, res, next) => {
+  const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  // Passing the error to the , error middleware handler
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
