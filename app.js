a61411b54e7988347136a3754fcd079448a1fc9d
eldrige <peldrige8@gmail.com>
2021-08-10 22:20:24 +0100

    refact: create error handling controller

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -16,21 +18,9 @@ app.get('/', (req, res) => {
 });
 
 app.all('*', (req, res, next) => {
-  const err = new Error(`Cant find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  // Passing the error to the , error middleware handler
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrHandler);
 
 module.exports = app;
