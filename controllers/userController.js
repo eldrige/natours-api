005e129925bb2373a69acc279422b45d25820866
eldrige <peldrige8@gmail.com>
2021-08-30 00:00:34 +0100

    update loggedin user func added

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+const updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password)
+    return next(new AppError('This route isnt for password updates', 400));
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+module.exports = {
+  updateMe,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
   resetPassword,
   updatePassword,
 } = require('../controllers/authController');
+const { updateMe } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -14,6 +15,7 @@ router.post('/signup', signUp);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
+router.patch('/updateMe', protect, updateMe);
 router.patch('/resetPassword/:token', resetPassword);
 
 module.exports = router;
