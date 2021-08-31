232add0d40a942344f9754eb65785f04f660302b
eldrige <peldrige8@gmail.com>
2021-08-31 18:34:20 +0100

    done with limits

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrHandler = require('./controllers/errorController');
 
@@ -19,6 +22,26 @@ const limiter = rateLimit({
 
 app.use(helmet());
 app.use(express.json({ limit: '10kb' })); // do not recieve request more than 10kb
+// Prevent against noSql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQty',
+      'average',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 app.use(morgan('dev'));
 app.use('/api', limiter); // apply rate limiting to all routes, starting with api
 
