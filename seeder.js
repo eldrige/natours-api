1bc5ca9b798588b15b89f206806b36c7a26bbb1f
eldrige <peldrige8@gmail.com>
2021-09-06 10:42:28 +0100

    update seeder script

--- seeder.js
+++ seeder.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const Tour = require('./models/Tour');
+const Review = require('./models/Review');
+const User = require('./models/User');
+
 const connectDB = require('./config/db');
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
 );
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
+);
+
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/reviews.json`)
+);
+
 connectDB();
 
 const importData = async () => {
   try {
+    await User.create(users);
+    await Review.create(reviews);
     await Tour.create(tours);
+    // await Tour.create(tours, {validateBeforeSave: false});  to skip validation rules before saving
     console.log('Data succesfully loaded');
   } catch (e) {
     console.log(e.message);
@@ -20,6 +35,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany({});
+    await Review.deleteMany({});
+    await User.deleteMany({});
     console.log('Data succesfully deleted');
   } catch (e) {
     console.log(e);
